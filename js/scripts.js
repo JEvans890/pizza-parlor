e192408d0a63f98d4644cb587cac7008773367c4
JEvans890 <evansjexp@gmail.com>
2023-06-16 09:56:55 -0700

    initialize framework of files with example of constructor

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function factoryFunction(radius) {
+    return {
+        radius,
+        draw() {
+            console.log('draw');
+        }
+
+    };
+}
+//Constructor Function
+function Circle(radius){
+    this.radius = radius; //empty object
+    this.draw = function(){
+        console.log('draw');
+    }
+//return this;
+}
+const circle = new Circle(1);
+//const x = {}; <-this.
